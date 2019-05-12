52be8067bd9d5cfee79243e51aff46f960e3264b
James Rogol <james.rogol@gmail.com>
2019-05-12 12:07:01 -0400

    Added Presto Sports roster scraper.

--- R/roster_Presto.R
+++ R/roster_Presto.R
@@ -1,26 +1,47 @@
-link <- "http://vcuathletics.com/sports/bsb/2018-19/roster"
-
-page <- xml2::read_html(link)
-
-players <- page %>% 
-  rvest::html_node("div.roster") %>% 
-  rvest::html_node("table") %>% 
-  rvest::html_table()
-
-players %>% 
-  dplyr::mutate_all(~gsub("^.+?:","",.)) %>% 
-  dplyr::mutate_all(~gsub("\\s+"," ",.)) %>% 
-  tidyr::separate("B/T", into = c("Bats","Throws"),
-                  sep = "/") %>% 
-  tidyr::extract("Hometown/Previous School",into = c("Hometown","State"),
-                 "(^[\\w -]+), ([\\w, -\\.]+)(?= /)") %>% 
-  # Format height appropriately.
+getRoster_Presto <- function(teamName, url, sport) {
+  roster <- fetchRoster_Presto(teamName, url, sport)
+  
+  rosterClean <- cleanRoster_Presto(roster)
+  
+  return(rosterClean)
+}
 
 
-dplyr::mutate(Year = stringr::str_replace(Cl.,"Fr.", "Freshman") %>% 
-                stringr::str_replace("So." , "Sophomore") %>% 
-                stringr::str_replace("Jr.", "Junior") %>% 
-                stringr::str_replace("Sr.", "Senior") %>% 
-                stringr::str_replace("Gr.", "Graduate Student") %>% 
-                stringr::str_replace("R-|Redshirt ?", "RS "),
-              Height = stringr::str_replace(Ht., "(\\d)-(\\d+)","\\1'\\2\\\""))
+fetchRoster_Presto <- function(teamName, url, sport){
+  page <- xml2::read_html(url)
+  
+  players <- page %>% 
+    rvest::html_node("div.roster") %>% 
+    rvest::html_node("table") %>% 
+    rvest::html_table()
+  
+  return(players)
+}
+
+
+cleanRoster_Presto <- function(rosterTable){
+  roster <- dplyr::mutate_all(rosterTable,~gsub("^.+?:","",.))
+  
+  roster <- dplyr::mutate_all(roster, ~gsub("\\s+"," ",.))
+  roster <- tidyr::separate(roster, "B/T", into = c("Bats","Throws"),
+                      sep = "/")  
+  roster <- tidyr::extract(roster, "Hometown/Previous School",into = c("Hometown","State"),
+                     "(^[\\w -]+), ([\\w, -\\.]+)(?= /)")
+      # Format height appropriately.
+  roster <- dplyr::mutate(roster, Cl. = stringr::str_replace(Cl.,"Fr.", "Freshman") %>% 
+                    stringr::str_replace("So." , "Sophomore") %>% 
+                    stringr::str_replace("Jr.", "Junior") %>% 
+                    stringr::str_replace("Sr.", "Senior") %>% 
+                    stringr::str_replace("Gr.", "Graduate Student") %>% 
+                    stringr::str_replace("R-|Redshirt ?", "RS "),
+                  Ht. = stringr::str_replace(Ht., "(\\d)-(\\d+)","\\1'\\2\\\""))
+    
+  roster <- roster %>% 
+      dplyr::rename(Year = Cl.,
+                    Height = Ht.,
+                    Weight = Wt.,
+                    Position1 = Pos.,
+                    Number = No.)
+  
+  return(roster)
+}
