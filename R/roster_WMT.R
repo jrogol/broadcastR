5f6b54260b2da3c60125f1cee02ac08b0f157421
James Rogol <james.rogol@gmail.com>
2022-01-30 18:21:00 -0500

    Error handling for missing Batting/Throwing in WMT rosters.

--- R/roster_WMT.R
+++ R/roster_WMT.R
@@ -27,20 +27,21 @@ fetchRoster_WMT <- function(teamName, url, sport){
 #' @return
 #'
 #' @examples
-cleanRoster_WMT <- function(roster_df){
+cleanRoster_WMT <- function(roster_df) {
   # Split bats into bats/throws, hometown into hometown/state, remove lbs from weight.
   
-  player_df <- try(tidyr::separate(roster_df,
-                                   `B/T`,
-                                   into = c("Bats", "Throws"),
-                                   sep = "[/-]")) %>%
-    rename_all(stringr::str_to_title) %>% 
-    dplyr::rename_at(dplyr::vars(dplyr::starts_with("Pos",ignore.case = T)),  ~ "Position") %>%
+  player_df <- purrr::possibly(tidyr::separate,
+                               otherwise = roster_df)(roster_df,
+                                                      `B/T`,
+                                                      into = c("Bats", "Throws"),
+                                                      sep = "[/-]") %>%
+    rename_all(stringr::str_to_title) %>%
+    dplyr::rename_at(dplyr::vars(dplyr::starts_with("Pos", ignore.case = T)),  ~ "Position") %>%
     tidyr::separate("Hometown",
                     into = c("Hometown", "State"),
                     sep = ", +") %>%
     dplyr::rename(dplyr::any_of(c(Height = "Ht.",
-                                  Weight = "Wt."))) %>% 
+                                  Weight = "Wt."))) %>%
     dplyr::mutate(
       Position1 = stringr::str_extract(Position, "[A-z/123]+$"),
       Height = stringr::str_replace(Height, "(\\d)-(\\d+)", "\\1'\\2\\\"")
@@ -48,7 +49,7 @@ cleanRoster_WMT <- function(roster_df){
     dplyr::select(-Position)
   
   return(dplyr::as_tibble(player_df))
-
+  
 }
 
 
