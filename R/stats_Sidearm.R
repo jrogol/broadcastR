d296acdceb79d561460e3bc9bceb1bd0215346cc
James Rogol <james.rogol@gmail.com>
2023-03-05 20:59:14 -0500

    Updated cleaning of Sidearm stats to handle new format.

--- R/stats_Sidearm.R
+++ R/stats_Sidearm.R
@@ -14,6 +14,13 @@ getStats_Sidearm <- function(statsURL, player_df, ...){
 fetchStats_Sidearm <- function(url){
   page <- xml2::read_html(url)
   
+  if(is.na(rvest::html_node(page,"table"))) {
+    message("Loading Page Found, Starting Selenium")
+    selStats <- fetch_SeleniumStats(url)
+    
+    return(selStats)
+  }
+  
   battingSection <- page %>% 
     rvest::html_node("section#individual-overall-batting,div#individual-batting")
   
@@ -60,9 +67,13 @@ cleanPlayer_SidearmStats <- function(table, col = "Player") {
 
 cleanBatting_Sidearm <- function(listTable,...){
   batting <- listTable$batting %>% 
-    tidyr::separate("GP-GS", into = c("GP","GS"), convert = T) %>% 
     tidyr::separate("SB-ATT", into = c("SB","ATT"), convert = T) %>% 
     dplyr::mutate(CS = ATT-SB)
+  
+  if(any(names(batting) == "GP-GS")){
+    batting <- batting %>% 
+      tidyr::separate("GP-GS", into = c("GP","GS"), convert = T)
+  }
   
   batting <- batting %>% 
     dplyr::filter(!is.na(AB),
@@ -75,12 +86,25 @@ cleanBatting_Sidearm <- function(listTable,...){
 
 cleanPitching_Sidearm <- function(listTable,...) {
   pitching <- listTable$pitching %>% 
-    tidyr::separate("W-L", into = c("Win","Loss"),
-                    sep = "-") %>% 
-    tidyr::separate("APP-GS",
-                    into = c("G","GS"),
-                    sep = "-") %>% 
     dplyr::mutate(SHO = stringr::str_remove(SHO,"-.*"))
+  
+  if("W-L" %in% names(pitching)){
+    pitching <- tidyr::separate(pitching,
+                                "W-L",
+                                into = c("Win","Loss"),
+                                sep = "-")
+  }
+  if("APP-GS" %in% names(pitching)){
+    pitching <- tidyr::separate(pitching,
+                                "APP-GS",
+                                into = c("G","GS"),
+                                sep = "-")
+  }
+  
+  names(pitching)[names(pitching) == "GP"] <- "G"
+  
+  wl <- grepl("WIN|LOSS",names(pitching))
+  names(pitching)[wl] <- stringr::str_to_title(names(pitching)[wl])
   
   
   pitching <- pitching %>% 
